adba5a10b245fbc9aeeac4c86444d97e7b6ed1ce
PoolloverNathan <nathan.kulzer@protonmail.com>
2023-08-30 22:15:51 -0400

    mild refactoring

--- src/client/kotlin/poollovernathan/fabric/endcables/CableRenderer.kt
+++ src/client/kotlin/poollovernathan/fabric/endcables/CableRenderer.kt
@@ -1,23 +1,16 @@
 package poollovernathan.fabric.endcables
 
 import net.minecraft.client.model.ModelPart
-import net.minecraft.client.render.BufferBuilder
 import net.minecraft.client.render.RenderLayer
 import net.minecraft.client.render.VertexConsumerProvider
-import net.minecraft.client.render.VertexConsumerProvider.Immediate
 import net.minecraft.client.render.block.entity.BlockEntityRenderer
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory
-import net.minecraft.client.render.model.BakedQuad
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.state.property.Properties
-import net.minecraft.util.math.Direction
 import net.minecraft.util.math.Direction.*
 import net.minecraft.util.math.Direction.Axis.*
 import net.minecraft.util.math.Direction.AxisDirection.*
-import net.minecraft.util.math.Vec3f
-import net.minecraft.util.shape.VoxelShapes
-import java.nio.Buffer
 import kotlin.math.sign
 
 class CableRenderer private constructor(private val ctx: BlockEntityRendererFactory.Context?) : BlockEntityRenderer<CableEntity> {
@@ -28,7 +21,7 @@ class CableRenderer private constructor(private val ctx: BlockEntityRendererFact
 
         override fun create(ctx: BlockEntityRendererFactory.Context?): BlockEntityRenderer<CableEntity> = CableRenderer(ctx)
 
-        val packetPart = ModelPart(listOf(ModelPart.Cuboid(0, 0, 8.0f, 8.0f, 8.0f, 0f, 0f, 0f, 2f, 2f, 2f, false, 1f, 1f)), mapOf())
+        val packetPart; get(/* for debugging */) = ModelPart(listOf(ModelPart.Cuboid(10, 10, 8.0f, 8.0f, 8.0f, 0f, 0f, 0f, 2f, 2f, 2f, false, 1f, 1f)), mapOf())
         fun innerPart(axis: Axis) =
             (3.0f to 7.999f).run {
                 when (axis) {
@@ -48,44 +41,29 @@ class CableRenderer private constructor(private val ctx: BlockEntityRendererFact
         light: Int,
         overlay: Int
     ) {
-        entity?.packet?.also {
-            val color = it.getColor()
-            matrices?.push {
-                val solid = vertexConsumers?.getBuffer(RenderLayer.getSolid())
-                var progress = when (((entity.world?.time ?: 0) - entity.insertionTime).sign) {
-                    0 -> tickDelta
-                    1 -> 1f
-                    -1 /* time-travel? */ -> 0f
-                    else -> 0f
+        entity?.apply {
+            packet?.apply {
+                var color = getColor()
+                color = Color.white()
+                matrices?.push {
+                    vertexConsumers?.render(RenderLayer.getSolid()) {
+                        var progress = when (((world?.time ?: 0) - insertionTime).sign) {
+                            0                     -> tickDelta
+                            1                     -> 1f
+                            -1 /* time-travel? */ -> 0f
+                            else                  -> 0f
+                        }
+                        progress = ((world!!.time % 4) + tickDelta) / 4 % 1
+                        progress -= 0.5f
+                        if (!forward) progress *= -1f
+                        matrices.translate(cachedState[Properties.AXIS].towards(POSITIVE).unitVector * progress)
+                        packetPart.render(matrices, it, 15, 15, color.rF, color.gF, color.bF, color.aF)
+                    }
                 }
-                progress -= 0.5f
-                if (!entity.forward) progress *= -1f
-                progress *= 16f
-                matrices.translate(0.5, 0.5, 0.5)
-                matrices.translate(entity.cachedState[Properties.AXIS].towards(POSITIVE).unitVector * progress)
-                packetPart.render(matrices, solid, 15, 0, color.rF, color.gF, color.bF, color.aF)
-                (vertexConsumers as Immediate).drawCurrentLayer()
+            }
+            vertexConsumers?.render(RenderLayer.getEndPortal()) {
+                innerPart(cachedState[Properties.AXIS]).render(matrices, it, 15, 0)
             }
         }
-        entity?.also {
-            val portal = vertexConsumers?.getBuffer(RenderLayer.getEndGateway())
-            innerPart(it.cachedState[Properties.AXIS]).render(matrices, portal, 15, 0)
-            (vertexConsumers as Immediate).drawCurrentLayer()
-        }
-    }
-}
-
-fun MatrixStack.translate(offset: Vec3f) = translate(offset.x.toDouble(), offset.y.toDouble(),
-    offset.z.toDouble()
-)
-
-operator fun Vec3f.times(scale: Float) = Vec3f(x * scale, y * scale, z * scale)
-
-fun MatrixStack.push(block: (MatrixStack.Entry) -> Unit) {
-    push()
-    try {
-        block(peek())
-    } finally {
-        pop()
     }
 }
---
+++ src/client/kotlin/poollovernathan/fabric/endcables/UtilClient.kt
@@ -0,0 +1,23 @@
+package poollovernathan.fabric.endcables
+
+import net.minecraft.client.render.RenderLayer
+import net.minecraft.client.render.VertexConsumer
+import net.minecraft.client.render.VertexConsumerProvider
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.util.math.Vec3f
+
+fun MatrixStack.translate(offset: Vec3f) = translate(offset.x.toDouble(), offset.y.toDouble(), offset.z.toDouble())
+inline fun MatrixStack.push(block: (MatrixStack.Entry) -> Unit) {
+    push()
+    try {
+        block(peek())
+    } finally {
+        pop()
+    }
+}
+
+inline fun VertexConsumerProvider.render(layer: RenderLayer?, draw: (VertexConsumer) -> Unit) {
+    val buffer = getBuffer(layer)
+    draw(buffer)
+    (this as VertexConsumerProvider.Immediate).drawCurrentLayer()
+}
